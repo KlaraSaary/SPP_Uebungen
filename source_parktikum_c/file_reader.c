518793d322e00b3f5e975ceaba7ecf504c8ed603
klara <klara.saary@gmail.com>
2016-12-03 13:28:14 +0100

    verweis error

--- source_parktikum_c/file_reader.c
+++ source_parktikum_c/file_reader.c
@@ -1,24 +1,49 @@
-#include "../header_praktikum_c/file_reader.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
+#include "../header_praktikum_c/list.h"
+#include <math.h>
 
-int main(){
-	char b = read_text_file("text1.txt", 0);
-	printf("This is block: %s \n", b);
-}
+int main() {
 
-char read_text_file(const char* filename, int blockSize){
-	FILE *text;
-	char block[16000];
-	char* p = block;
-	text = fopen(filename, "r");
+	FILE* text;
+	int  blocksize = 500;
+	text = fopen("text1.txt", "r");
 	if(text == NULL){
-      		perror("Error opening file");
-      		return(-1);
-   	}
-	strcat(block, fgets(block, 500, (FILE*)text));
+		perror("Error opening file");
+		return(-1);
+	}
+	fseek(text, 0, SEEK_END);
+	long size = ftell(text);
+	fseek(text, 0, SEEK_SET);
+	int s = (int) round(size/blocksize + 0.5);
+	char* a[s];
+	char temp[500];
+	char* b;
+	b =(char*) malloc(blocksize);
+	*b = '\0';
+	LinkedList* list = LinkedList_create();
+	int i = blocksize;
+	int j = 0;
+	while(fgets(temp,i,text) != NULL){
+		i = blocksize - strlen(b);
+		if(i >= strlen(temp)){
+			strcat(b, temp);
+		}
+		else{
+			a[j] = (char*) malloc(sizeof(char)*strlen(b));
+			strcpy(a[j], b);
+			LinkedList_append(list, a[j]);
+			*b = '\0';
+		} 
+		printf("%s \n",a[j]);
+		/*printf("%i \n",(int) strlen(b));
+		fgets(temp,i,text);
+		strcat(b, temp);*/
+		j++;
+	}
 	fclose(text);
-	return block;
+	//printf("%s \n",b);
+	free(b);
+	return 0;
 }
