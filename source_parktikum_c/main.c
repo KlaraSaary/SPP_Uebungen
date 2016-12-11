3a151c87179de9830add307971fb44f16b0c7eb0
Felix Staniek <staniek@sim.tu-darmstadt.de>
2016-12-11 23:58:25 +0100

    2a parallelism

--- source_parktikum_c/main.c
+++ source_parktikum_c/main.c
@@ -8,6 +8,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <omp.h>
+#include <time.h>
 
 //main file_reader.c
 /*
@@ -99,17 +100,17 @@ int main(){
 	Dictionary_insert(dict, absolut);
 	Dictionary_print(dict);
 	int i = Dictionary_isIn(dict, ab);
-	printf("main i : %i \n", i);*/
-
+	printf("main i : %i \n", i);
+*/
 
 	Dictionary* dict_ = Dictionary_create();
 	LinkedList* reference_ = LinkedList_create();
 	reference_ = read_text_file("/home/felix/git/spp/SPP_Uebungen/source_parktikum_c/text1.txt",16000);
 	LinkedList* testtext_= LinkedList_create();
-	testtext_ = read_text_file("/home/felix/git/spp/SPP_Uebungen/source_parktikum_c/text3.txt",16000);
+	testtext_ = read_text_file("/home/felix/git/spp/SPP_Uebungen/source_parktikum_c/text4.txt",16000);
 
 	char* parserpointer1_ = malloc(sizeof(char));
-	char* parserpointer2_ = malloc(sizeof(char));
+
 
 	Parser* parser1_ ;
 	Parser* parser2_ ;
@@ -120,35 +121,49 @@ int main(){
 		parser1_ = Parser_create(LinkedList_getData(acctualnode1_));
 		while(Parser_getNextWord(parser1_,parserpointer1_,20)!=0){
 			Dictionary_insert(dict_,parserpointer1_);
-			parserpointer1_ = malloc(sizeof(char)*12);
+			parserpointer1_ = malloc(sizeof(char)*20);
 
 		}
 		acctualnode1_ = LinkedList_getNext(acctualnode1_);
 	}
-	Dictionary_print(dict_);
-	int counter_=0 ;
+
+
 
 	LinkedListNode* acctualnode2_ = LinkedList_getFirst(testtext_);
+
+	int counter_=0 ;
  	int i =0;
  	int counter2=0;
  	double time = omp_get_wtime( );
-	#pragma omp parallel for private(counter2)\ reduction(+: counter_)
+	#pragma omp parallel for private(i) reduction(+: counter_) num_threads(4)
+
+
 	for(i=0;i<LinkedList_getSize(testtext_);i++){
 
+		//int tid = omp_get_thread_num();
+		//printf("Hello World from thread = %d\n", tid);
+		//printf("%d\n", omp_get_thread_num());
 		parser2_ = Parser_create(LinkedList_getDataAt(testtext_,i));
-				while(Parser_getNextWord(parser2_,parserpointer2_,12)!=0){
+		char* parserpointer2_ = malloc(sizeof(char));
+				//#pragma omp parallel reduction(+:counter_)
+
+				while(Parser_getNextWord(parser2_,parserpointer2_,20)!=0){
+
 						if(!Dictionary_isIn(dict_, parserpointer2_)){
-							Dictionary_insert(dict_,parserpointer1_);
-							counter2++;
-							counter_ +=counter2;
+							//printf("%s\n",parserpointer2_);
+							Dictionary_insert(dict_,parserpointer2_);
+							counter_++;
+							//
 
 						}
+
 				}
 				//acctualnode2_ = LinkedList_getNext(acctualnode2_);
 
-	}
+	};
 
 
+/*
 	while(acctualnode2_ != NULL){
 		parser2_ = Parser_create(LinkedList_getData(acctualnode2_));
 		while(Parser_getNextWord(parser2_,parserpointer2_,12)!=0){
@@ -159,9 +174,12 @@ int main(){
 		}
 		acctualnode2_ = LinkedList_getNext(acctualnode2_);
 	}
+	*/
 	printf("Time: \t %f \n", omp_get_wtime()-time);
-	printf("%i words are found exclusively in the second text!", counter_);
+	//Dictionary_print(dict_);
+	printf("%i words are found exclusively in the second text! sould be 3808 \n", counter_);
 	return 0;
+
 
 };
 
