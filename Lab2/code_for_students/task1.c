704f85cb2b5189faaa9612e38dc59d2fd262eaf7
Felix Staniek <staniek@sim.tu-darmstadt.de>
2017-01-15 19:59:05 +0100

    working task1.c

--- Lab2/code_for_students/task1.c
+++ Lab2/code_for_students/task1.c
@@ -14,21 +14,39 @@ int compare (const void * a, const void * b)
    return ( *(int*)a - *(int*)b );
 }
 
+void print(int num,int* arr, int local_len){
+	int o;
+	printf("process %d: [",num);
+	for(o=0;o<local_len;o++){
+		printf("%d ",arr[o]);
+	}
+	printf("] %d\n",o);
+}
 void merge_arr( int* arr1, int n1, int* arr2, int n2, 
                 int* tmp_arr ) {
+	
+	//print(13,arr2,n2);
+	
+	//printf("%d long array arr1 is",n1);
+	
 	int i = 0, j = 0, k = 0;
+	//printf(" = ");
     	while (i<n1 && j<n2){
+			//rintf("%d ",arr1[i]);
         	if (arr1[i] < arr2[j]){
 	            tmp_arr[k] = arr1[i];
+				//printf("case1 because %d is smaler as %d at %d and %d\n",arr1[i],arr2[j],i,j);
 	            i++;
         	}
         	else{
 	            tmp_arr[k] = arr2[j];
 	            j++;
+				//printf("case2\n");
         	}
 	        k++;
     	}
 
+	
     	while (i<n1){
         	tmp_arr[k] = arr1[i];
 	        	i++;
@@ -45,25 +63,6 @@ void merge_arr( int* arr1, int n1, int* arr2, int n2,
 	
 }
 
-void print(int num,int* arr, int local_len){
-	int o;
-	printf("process %d: [",num);
-	for(o=0;o<local_len;o++){
-		printf("%d ",arr[o]);
-	}
-	printf("]\n");
-}
-
-void fill(int* arr,int n, int** buf){
-
-	//print(1234,*buf,n);
-	int d;
-	for(d=0;d<n;d++){
-			//printf("%d and \n",arr[d]);
-				//*buf[d]=arr[d];
-				//printf("%d\n",*buf[d]);
-	}
-}
 
 /**
  * Do the parallel sorting
@@ -71,103 +70,52 @@ void fill(int* arr,int n, int** buf){
 void par_sort( int** orig_arr, int* orig_len, int myrank, 
                int nprocs ) {
 
-	int n = *orig_len/nprocs;
-	int z,t,q,position,d,i;
+	int height=log2(nprocs)+1;
+	int z,t,q,position,d,i,layer;
 	int* sendbuf = malloc(*orig_len*sizeof(int));
-	int* sendbuf3 = malloc(*orig_len*sizeof(int));
-	int* sendbuf2 = malloc(*orig_len*sizeof(int));
+	int* sendbuf3 = malloc(nprocs*(*orig_len)*sizeof(int));
+	
+	
 	int* solution = malloc(*orig_len*sizeof(int));
 	int* proc_i=malloc(*orig_len*sizeof(int));
 	int* proc_z=malloc(*orig_len*sizeof(int));
+	layer=1;
+	
+	//sort and first merge
 	
 	
-	//print(12,*orig_arr,*orig_len);
-	
-	
-	if(myrank==0){
-	
-	//printf("rank 0 successfully called\n");
-	sendbuf=*orig_arr;
-	
-	//print(12,*orig_arr,n,&sendbuf);
-	print(12,*orig_arr,*orig_len);
-	for(d=0;d<n;d++){
-		
-			printf("%d\n",sendbuf[d]);
-				//*buf[d]=arr[d];
-				//printf("%d\n",*buf[d]);
+	if(myrank%2!=0){
+			qsort(*orig_arr,*orig_len,sizeof(int),compare);
+			sendbuf=*orig_arr;
+			//print(myrank,sendbuf,*orig_len);
+			MPI_Send(sendbuf,*orig_len,MPI_INT,myrank-1,myrank,MPI_COMM_WORLD);
 	}
-		
-	
-		
-	//print(myrank,sendbuf,n);
-		
-	position=0;
-		
-		for(i=0;i<nprocs;i++){
-			for(d=0;d<n;d++){
-				sendbuf3[d]=sendbuf[position+d];
-				//printf("%d\n",sendbuf[d]);
-			}
+	else{
+			qsort(*orig_arr,*orig_len,sizeof(int),compare);
+			//print(myrank,sendbuf,*orig_len);
+	}
 			
-			MPI_Send(sendbuf3,n,MPI_INT,i,0,MPI_COMM_WORLD);
-			position += n;
+	
+	//now go through all layers and merge parralel
+	
+	t=1;
+	while(t<height){
+		if(myrank%(int)pow(2,t)==0){
+			printf("%d revc from %d; orig_arr: %d  snedbuf5: %d\n",myrank,myrank+(int)pow(2,(t-1)),*orig_len*(int)pow(2,t-1),*orig_len*(int)pow(2,t));
+			int* sendbuf2 = malloc(4*(*orig_len)*sizeof(int));
+			int* sendbuf5 = malloc(4*(*orig_len)*sizeof(int));
+			MPI_Recv(sendbuf5,*orig_len*(int)pow(2,t-1),MPI_INT,myrank+(int)pow(2,(t-1)),myrank+(int)pow(2,(t-1)),MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			merge_arr(*orig_arr,*orig_len*(int)pow(2,t-1),sendbuf5,*orig_len*(int)pow(2,t-1),sendbuf2);
+			*orig_arr=sendbuf2; //pass merged value into orig_arr
+				if(myrank%(int)pow(2,t+1)!=0){
+				sendbuf5 = *orig_arr;
+				MPI_Send(sendbuf5,*orig_len*(int)pow(2,t),MPI_INT,myrank-(int)pow(2,(t)),myrank,MPI_COMM_WORLD);
+				}
 		}
-	
-		
-		i=1;
-		
-			//printf("%d\n",i);
-		for(q=0;q<n;q++){
-			proc_i[q]=sendbuf[q];	
-		}
-		qsort(proc_i,n,sizeof(int),compare);
-			//printf("here comes the plane #%d ",i);
-			//print(11,proc_i,*orig_len);
-		//for(t=0;t<log2(n);t++){
-		while(i<4){
-		MPI_Recv(sendbuf2,*orig_len,MPI_INT,i,i,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			for(q=0;q<*orig_len;q++){
-				proc_z[q]=sendbuf2[q];
-			}
-		merge_arr(proc_i,i*n,proc_z,n,solution);
-			for(q=0;q<*orig_len;q++){
-				proc_i[q]=solution[q];
-			}
-		i++;	
-		}
-		
-		
-		
-		print(9999, proc_i,*orig_len);
-		*orig_arr=proc_i;	
-	
-	}else{
-	//printf("%d calles successfully\n",myrank);	
-		
-		MPI_Recv(sendbuf3,n,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		qsort(sendbuf3,n,sizeof(int),compare);
-		for(i=0;i<n;i++){
-			sendbuf2[i]=sendbuf3[i];
-		}
-		MPI_Send(sendbuf2,n,MPI_INT,0,myrank,MPI_COMM_WORLD);
-		//print(myrank,sendbuf2,n);
-			
-		
-	
-	
-	
-	
+	t++;
 	}
 	
 }
-    /* Allocate temp arrays */
-	
-    
-    // Use merge_arr()
-    
-
-    /* Free memory */
 
 
 int is_arr_sorted( int* arr, int len ) {
@@ -208,8 +156,9 @@ int main( int argc, char** argv ) {
 
     /* Verify the results */
     if( myrank == 0 ) {
+		print(666,local_arr,nprocs*local_len);
         int res = is_arr_sorted( local_arr, local_len );
-       // printf( "Sorted: %d\n", res );
+        printf( "Sorted: %d\n", res );
     }
 
     free( local_arr );
